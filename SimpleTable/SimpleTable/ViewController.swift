59de1579109ce883b6696bd52276c7aa336247b2
gnuhua <gnu.hua@gmail.com>
2016-04-21 23:41:32 +0800

    New Simple TableView Application

---
+++ SimpleTable/SimpleTable/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SimpleTable
+//
+//  Created by huatian on 16/4/21.
+//  Copyright © 2016年 iosdevlog. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
